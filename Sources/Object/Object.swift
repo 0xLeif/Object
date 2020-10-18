73721b551496ab56957d90f464673f0e7635c471
zmeriksen <zmeriksen@icloud.com>
2020-10-18 12:20:02 -0500

    Added modify, set, and update

--- Sources/Object/Object.swift
+++ Sources/Object/Object.swift
@@ -48,16 +48,60 @@ public class Object {
         return object
     }
     /// Add a Value with a name to the current object
-    public func add(variable named: AnyHashable = "_value", value: Any) {
+    @discardableResult
+    public func add(variable named: AnyHashable = "_value", value: Any) -> Self {
         variables[named] = value
+        
+        return self
+    }
+    /// Modify a Value with a name to the current object
+    @discardableResult
+    public func modify<T>(variable named: AnyHashable = "_value", modifier: (T?) -> T?) -> Self {
+        guard let variable = variables[named],
+              let value = variable as? T else {
+            variables[named] = modifier(nil)
+            
+            return self
+        }
+        variables[named] = modifier(value)
+        
+        return self
+    }
+    /// Set a Value with a name to the current object
+    @discardableResult
+    public func set<T>(variable named: AnyHashable = "_value", modifier: (T) -> T?) -> Self {
+        guard let variable = variables[named],
+              let value = variable as? T else {
+            return self
+        }
+        variables[named] = modifier(value)
+        
+        return self
+    }
+    /// Update a Value with a name to the current object
+    @discardableResult
+    public func update<T>(variable named: AnyHashable = "_value", modifier: (T) -> T) -> Self {
+        guard let variable = variables[named],
+              let value = variable as? T else {
+            return self
+        }
+        variables[named] = modifier(value)
+        
+        return self
     }
     /// Add a ChildObject with a name of `_object` to the current object
-    public func add(childObject object: Object) {
+    @discardableResult
+    public func add(childObject object: Object) -> Self {
         variables["_object"] = object
+        
+        return self
     }
     /// Add an Array with a name of `_array` to the current object
-    public func add(array: [Any]) {
+    @discardableResult
+    public func add(array: [Any]) -> Self {
         variables["_array"] = array
+        
+        return self
     }
     /// Add a Function with a name and a closure to the current object
     public func add(function named: AnyHashable, value: @escaping ObjectFunction) {
